2aeda17be591f36f46afcfefbeb15cbb21365436
Sergey Matiyasevich <FoxRandom@Foxs-MacBook-Pro.local>
2014-06-22 14:30:29 +0400

    First upload

---
+++ js/main.js
@@ -0,0 +1,27 @@
+function closePopup() {
+	document.getElementById('popup_bg').style.display = "none";
+	document.getElementById('floating_pan').style.display = "block";
+}
+function openPopup() {
+	document.getElementById('popup_bg').style.display = "block";
+	document.getElementById('floating_pan').style.display = "none";
+}
+function switchToWebcam() {
+	var video = document.querySelector("#videoElement");
+	navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
+ 
+	if (navigator.getUserMedia) {       
+	    navigator.getUserMedia({video: true}, handleVideo, videoError);
+		}
+ 
+	function handleVideo(stream) {
+   		video.src = window.URL.createObjectURL(stream);
+		}
+ 
+	function videoError(e) {
+    	alert('Karamba!');
+		}
+}
+function switchTo3d() {
+	alert('3D');
+}
